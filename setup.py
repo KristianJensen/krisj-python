82b1043de90a41324866655ebccf8d0461979590
KristianJensen <krisje@nnfcb-l0275.local>
2015-09-11 13:14:58 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='krisj',
+      version='0.1',
+      description='Kristians Python Utilities',
+      author='Kristian Jensen',
+      author_email='krisje@biosustain.dtu.dk',
+      url='http://github.com/KristianJensen/krisj-python/'
+     )
