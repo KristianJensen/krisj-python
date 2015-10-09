2ec82b6014efdd65ec84b30ee9d462cc6e8dc0a2
KristianJensen <krisje@nnfcb-l0275.local>
2015-10-09 11:31:56 +0200

    Merge with old stuff

--- krisj/plotting.py
+++ krisj/plotting.py
@@ -4,11 +4,10 @@ from __future__ import absolute_import, print_function
 
 import math
 import bokeh.plotting as bplt
-from bokeh.charts import Bar, Histogram, HeatMap
 import colorsys
 
 import numpy as np
-import pandas as pd
+import random
 
 
 def color_range(n, lightness=0.5, saturation=1, offset=0):
@@ -46,7 +45,6 @@ def dataframe_summary_plot(df, categorical_column, numerical_column, error_bars=
     stds = np.array(stds)
     sems = np.array(sems)
 
-
     if error_bars == "std":
         errors = stds
     elif error_bars == "sem":
@@ -57,7 +55,6 @@ def dataframe_summary_plot(df, categorical_column, numerical_column, error_bars=
     lowers = means - errors
     uppers = means + errors
 
-
     data = {numerical_column: (means, lowers, uppers)}
 
     fig = bar_plot_with_error_bars(data, categories=categories, **kwargs)
@@ -65,7 +62,7 @@ def dataframe_summary_plot(df, categorical_column, numerical_column, error_bars=
     return fig
 
 
-def bar_plot_with_error_bars(data, categories=None, colors=None, legend=True):
+def bar_plot_with_error_bars(data, categories=None, colors=None, legend=True, **kwargs):
     """
 
     :param data: Dict containing series. Each series containing a tuple of (means, lowers, uppers) each of length n_cats.
@@ -74,7 +71,8 @@ def bar_plot_with_error_bars(data, categories=None, colors=None, legend=True):
     n_series = len(data)
     n_cats = None
 
-    width = 0.5
+    column_width = 0.4
+    width = 0.4
 
     if colors is None:
         colors = color_range(n_series)
@@ -95,23 +93,23 @@ def bar_plot_with_error_bars(data, categories=None, colors=None, legend=True):
         if len(categories) != n_cats:
             raise ValueError("Length of 'categories' argument does not fit the data")
 
-    fig = bplt.figure(x_range=categories)
+    fig = bplt.figure(x_range=categories, legend=legend, **kwargs)
     xs = np.arange(1, n_cats+1)
 
-    series_centers = np.linspace(-0.5, 0.5, n_series+2)[1:-1]
+    series_centers = np.linspace(-width, width, n_series+2)[1:-1]
 
     for i, series in enumerate(data):
 
         means, lowers, uppers = map(np.array, (data[series]))
 
         if legend:
-            series_legend=series
+            series_legend = series
         else:
-            series_legend=None
+            series_legend = None
 
         fig.quad(bottom=[0]*n_cats, top=means,
-                 left=xs+series_centers[i]-width/(2*n_series),
-                 right=xs+series_centers[i]+width/(2*n_series), color=colors[i], legend=series_legend)
+                 left=xs+series_centers[i]-column_width/(2*n_series),
+                 right=xs+series_centers[i]+column_width/(2*n_series), color=colors[i], legend=series_legend)
 
         for j in range(n_cats):
             fig.line( [xs[j]+series_centers[i]]*2, [lowers[j], uppers[j]], color="black")
@@ -122,5 +120,79 @@ def bar_plot_with_error_bars(data, categories=None, colors=None, legend=True):
     return fig
 
 
+def make_categorical_scatter(data, color="#669900", percentiles=(), **kwargs):
+    """
+    :param data:
+    :return:
+    """
+    categories = list(data.keys())
+
+    fig = bplt.figure(title=None, x_range=categories, **kwargs)
+
+    fig.xaxis.major_label_text_font_size = "16px"
+
+    for i, name in enumerate(categories):
+        dat_array = np.array(data[name])
+        if 50 in percentiles or 50. in percentiles:
+            median = np.percentile(dat_array, 50)
+            fig.line([i+0.8, i+1.2],[median, median], color="black")
+        dat_list = list(data[name])
+        x_list = [i+1+random.gauss(0, 0.1) for _ in dat_list]
+        fig.circle(x_list, dat_list, fill_alpha=0.4, line_alpha=0,  fill_color=color)
+
+        for perc in percentiles:
+            percentile = np.percentile(dat_array, perc)
+            fig.line([i+0.9, i+1.1], [percentile, percentile], color="black")
+
+    return fig
 
 
+def radar_plot(df, title=None):
+    fig = bplt.figure(tools=["reset", "pan", "wheel_zoom", "save"], height=600,
+                      width=600, y_range=[-1.6, 1.6], x_range=[-1.6, 1.6], title=title)
+
+    fig.grid.grid_line_color = None
+    fig.axis.axis_line_color = None
+    fig.axis.major_tick_line_color = None
+    fig.axis.minor_tick_line_color = None
+    fig.axis.major_label_text_color = None
+    fig.outline_line_color = None
+
+    labels = df.columns
+    n = len(labels)
+    series = df.index
+    data = df.values
+    ticksize = 0.02
+    ticknum = 5
+
+    color_strings = color_range(len(data), 0.8)
+
+    spokes = []
+
+    max_scale = 0.5
+
+    # Draw spokes
+    for i in range(n):
+        end = (math.cos(2.*math.pi/n*i), math.sin(2.*math.pi/n*i))
+        spokes.append(end)
+        text_offset = len(labels[i])/16.*int(end[0] < 0)
+        fig.text(1.1*end[0]-text_offset, 1.1*end[1]-0.05, text=labels[i])
+        fig.line([0, end[0]], [0, end[1]])
+        # Draw ticks on spokes
+        for j in range(1, ticknum+1):
+            pt = end[0]/ticknum*j, end[1]/ticknum*j
+            up = pt[0]+end[1]*ticksize, pt[1]-end[0]*ticksize
+            down = pt[0]-end[1]*ticksize, pt[1]+end[0]*ticksize
+            text_loc = pt[0]-end[1]*3*ticksize-0.05, pt[1]+end[0]*3*ticksize-0.05
+            fig.line([up[0], down[0]], [up[1], down[1]])
+            fig.text(*text_loc, text=str(max_scale/ticknum*j), text_font_size="12px")
+
+    # Draw data
+    for j, d in enumerate(data):
+        xs = [d[i]*spokes[i][0]/max_scale for i in range(len(spokes))]
+        ys = [d[i]*spokes[i][1]/max_scale for i in range(len(spokes))]
+        fig.patch(x=xs, y=ys, color=color_strings[j], line_width=3, fill_alpha=0.1, legend=series[j])
+
+    #fig.circle(x=0, y=0, radius=1, fill_alpha=0)
+
+    return fig
---
+++ krisj/util.py
@@ -0,0 +1,396 @@
+# Copyright 2015 Novo Nordisk Foundation Center for Biosustainability, DTU.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# Various utility functions
+# Kristian Jensen, March 2015
+#
+
+import math
+import cameo
+import colorsys
+from cameo.util import TimeMachine
+from functools import partial
+from cameo import config
+import re
+import numpy as np
+import pandas as pd
+from Bio.Seq import Seq
+from Bio.SeqIO import SeqRecord
+import bokeh
+import sys
+from scipy import stats
+from cameo.exceptions import SolveError
+import bokeh.plotting as bplt
+import random
+
+
+def round_fva_bound(bound, ndecimals):
+    """
+    Rounds a bound away from zero to ndecimals precision.
+    """
+    if bound > 0:
+        bound = math.ceil(bound * 10**ndecimals)/10**ndecimals
+    elif bound < 0:
+        bound = math.floor(bound * 10**ndecimals)/10**ndecimals
+    return bound
+
+
+def apply_fva_constraints(model, fva_result, skip_zeros=False, skip_list=[]):
+    """
+    Applies the reaction bounds found with an FVA analysis to the model.
+    If skip_zeros is set to True, only reactions with non-zero FVA bounds are changed. Reversible
+    reactions that are only used in one direction in the FVA are only limited in that direction.
+    """
+    from cameo import config
+    ndecimals = config.ndecimals
+    exchanges = model.exchanges
+
+    for reaction_id in fva_result.index:
+        if skip_zeros:
+            if fva_result["upper_bound"][reaction_id] == 0 and fva_result["lower_bound"][reaction_id] == 0:
+                continue
+        reaction = model.reactions.get_by_id(reaction_id)
+        if reaction in exchanges or reaction in skip_list or reaction_id in skip_list or reaction_id == "ATPM":
+            pass
+        else:
+            if reaction.upper_bound > 0:
+                if fva_result["upper_bound"][reaction_id] <= 0:
+                    if skip_zeros is False:
+                        reaction.upper_bound = 0
+                else:
+                    rounded_bound = round_fva_bound(fva_result["upper_bound"][reaction_id], ndecimals)
+                    if reaction.upper_bound == 1000:
+                        reaction.upper_bound = rounded_bound
+                    else:
+                        reaction.upper_bound = max(rounded_bound, reaction.upper_bound)
+            if reaction.lower_bound < 0:
+                if fva_result["lower_bound"][reaction_id] >= 0:
+                    if skip_zeros is False:
+                        reaction.lower_bound = 0
+                else:
+                    rounded_bound = round_fva_bound(fva_result["lower_bound"][reaction_id], ndecimals)
+                    if reaction.lower_bound == -1000:
+                        reaction.lower_bound = rounded_bound
+                    else:
+                        reaction.lower_bound = min(rounded_bound, reaction.lower_bound)
+
+
+def binding_constraints(model, ignore_zeros=True):
+    """
+    Solves the model and returns a list of the reactions that are bound by a constraint.
+    :param model: A model
+    :param ignore_zeros: If True reactions that are constrained to zero will not be reported.
+    :return: A list of reactions
+    """
+    binding = []
+    fluxes = model.solve().fluxes
+    for reaction in model.reactions:
+        flux = fluxes[reaction.id]
+        if ignore_zeros:
+            if flux == 0:
+                continue
+        if flux == reaction.upper_bound or flux == reaction.lower_bound:
+            binding.append(reaction)
+    return binding
+
+
+def find_mutant_growth_rate(model, relative_bounds, original_bounds=None, skip_essential=False, solve_method=cameo.fba,
+                            essential_reactions=None, **simulation_kwargs):
+    """
+    Calculates the fluxes for a model where bounds have been modified.
+    :param model: SolverBasedModel
+    :param relative_bounds: A dictionary of relative bounds that will be applied to the bounds of each reaction
+    :original_bounds: A pandas dataframe (as output by cameo.flux_variability_analysis) containing initial bounds.
+        If None, an FVA will be run to determine them.
+    :param skip_essential: Reaction bounds will not be modified if relative bound is less than this parameter.
+        Set to False or None to disable skipping.
+    """
+    with TimeMachine() as tm:
+        # Make sure bounds are reset
+        for reaction in model.reactions:
+            tm(do=int, undo=partial(setattr, reaction, "upper_bound", reaction.upper_bound))
+            tm(do=int, undo=partial(setattr, reaction, "lower_bound", reaction.lower_bound))
+
+        # Find essential reactions
+        if skip_essential:
+            if essential_reactions is None:
+                essential_reactions = model.essential_reactions()
+
+        # Perform FVA
+        if original_bounds is None:
+            fva_result = cameo.flux_variability_analysis(model, fraction_of_optimum=1.0, remove_cycles=False)
+            fva_result["lower_bound"] = fva_result["lower_bound"].round(6)
+            fva_result["upper_bound"] = fva_result["upper_bound"].round(6)
+        else:
+            fva_result = original_bounds
+
+        # Set initial bounds
+        for reaction_id in fva_result.index:
+                reaction = model.reactions.get_by_id(reaction_id)
+                if fva_result["upper_bound"][reaction_id] >= 0:
+                    reaction.upper_bound = round_fva_bound(fva_result["upper_bound"][reaction_id], config.ndecimals)
+                if fva_result["lower_bound"][reaction_id] <= 0:
+                    reaction.lower_bound = round_fva_bound(fva_result["lower_bound"][reaction_id], config.ndecimals)
+
+        # Modify bounds
+        for reaction_id in relative_bounds:
+            reaction = model.reactions.get_by_id(convert_reaction(reaction_id))
+            if skip_essential and reaction in essential_reactions and round(relative_bounds[reaction_id], config.ndecimals) <= skip_essential:
+                #print "Skipped "+reaction_id
+                continue
+            if convert_reaction(reaction_id) not in fva_result.index:
+                raise Warning("Reaction "+reaction_id+" is not in the specified original bounds")
+            reaction.lower_bound *= relative_bounds[reaction_id]
+            reaction.upper_bound *= relative_bounds[reaction_id]
+
+        # Solve mutated model
+        #solution = model.solve()
+        solution = solve_method(model, **simulation_kwargs)
+    return solution
+
+
+def convert_reaction(reaction):
+    """
+    Converts a reaction id to cameo format.
+    :param reaction:
+    :return:
+    """
+    reaction = re.sub(r"-", "_dsh_", reaction)
+    reaction = re.sub(r"\(", "_lp_", reaction)
+    reaction = re.sub(r"\)", "_rp_", reaction)
+    return reaction
+
+
+def compare_flux_distributions(flux_dist_1, flux_dist_2, reactions, ndecimals=None):
+    result = []
+    if ndecimals is None:
+        ndecimals = config.ndecimals
+    for reaction in reactions:
+        try:
+            reac_id = reaction.id
+        except AttributeError:
+            reac_id = reaction
+
+        if round(flux_dist_1[reac_id], ndecimals) != round(flux_dist_2[reac_id], ndecimals):
+            result.append( (reac_id, flux_dist_1[reac_id], flux_dist_2[reac_id], flux_dist_1[reac_id]-flux_dist_2[reac_id]) )
+
+    if len(result) > 0:
+        return pd.DataFrame(result, columns=["reaction", "flux_1", "flux_2", "difference"])
+    else:
+        return pd.DataFrame(columns=["reaction", "flux_1", "flux_2", "difference"])
+
+
+def plot_predicted_growth_rates(predicted_solutions, experimental_growth_rate_filename, standardize=True, show_correlation=True):
+
+    gr_df = pd.read_csv(experimental_growth_rate_filename)
+
+    calc_growth_rates = {strain.split()[0]: solution.fluxes["Ec_biomass_iJO1366_core_53p95M"] for strain, solution in predicted_solutions.items()}
+
+    plot_df = gr_df.copy()
+    plot_df["calc"] = plot_df["strain"].map(calc_growth_rates.get)
+    plot_df = plot_df[plot_df["calc"].notnull()]
+
+    calc_mean = plot_df["calc"].values.mean()
+    calc_std = plot_df["calc"].values.std()
+
+    mean_mean = plot_df["mean"].values.mean()
+    mean_std = plot_df["mean"].values.std()
+
+    def standardize_calc(num):
+        if standardize:
+            return (num - calc_mean) / calc_std
+        else:
+            return num
+
+    def standardize_mean(num):
+        if standardize:
+            return (num - mean_mean) / mean_std
+        else:
+            return num
+
+    cor_coef = calculate_growth_correlation_coefficient(predicted_solutions, experimental_growth_rate_filename)[0]
+
+    if standardize:
+        x_range = [-2, 3]
+        y_range = [-2, 3]
+        text_spot = [-2.5, 2.5]
+    else:
+        x_range = [0, 1]
+        y_range = [-0.05, 1]
+        text_spot = [0.1, 0.9]
+
+    #x_range = None
+    #y_range = None
+
+    print plot_df["mean"].values.mean(), plot_df["mean"].values.std(), plot_df["calc"].values.mean(), plot_df["calc"].values.std()
+
+    slope, intercept, pearson, p_val, stderr = stats.linregress(plot_df["mean"].values, plot_df["calc"].values)
+
+    fig = bokeh.plotting.figure(title=None, x_range=x_range, y_range=y_range)
+
+    fig.scatter(standardize_mean(plot_df["mean"].values), standardize_calc(plot_df["calc"].values))
+    #fig.line(np.arange(0, 1, 0.1), (6.83)*np.arange(0, 1, 0.1)-3.74, color="red")
+    #fig.line(*([np.arange(-3,3,0.1)]*2), color="red")
+    for idx in plot_df.index:
+        mean = plot_df["mean"][idx]
+        std = plot_df["std"][idx]
+        n = plot_df["n"][idx]
+        sem = std / math.sqrt(n)
+        calc = standardize_calc(plot_df["calc"][idx])
+        low, high = standardize_mean(mean-sem), standardize_mean(mean+sem)
+        fig.line([low, high], [calc, calc])
+
+    if show_correlation:
+        fig.text(*text_spot, text="Corr. coefficient: "+str(round(cor_coef, 3)))
+    fig.xaxis.axis_label = "Exp"
+    fig.yaxis.axis_label = "Calc"
+
+    return fig
+
+
+def calculate_growth_correlation_coefficient(predicted_solutions, experimental_growth_rate_filename):
+    exps = []
+    calcs = []
+
+    gr_df = pd.read_csv(experimental_growth_rate_filename)
+    for idx, row in gr_df.iterrows():
+        strain = row[0]+" v1"
+        if strain in predicted_solutions:
+            exp = row[1]
+            calc = predicted_solutions[strain].fluxes["Ec_biomass_iJO1366_core_53p95M"]
+            exps.append(exp)
+            calcs.append(calc)
+
+    cor_coef = np.corrcoef(np.array(exps), np.array(calcs))[0, 1]
+    p_val = stats.pearsonr(np.array(exps), np.array(calcs))[1]
+    return cor_coef, p_val
+
+
+def calculate_mean_squared_error(predicted_solutions, experimental_growth_rate_filename):
+    exps = []
+    calcs = []
+
+    gr_df = pd.read_csv(experimental_growth_rate_filename)
+    for idx, row in gr_df.iterrows():
+        strain = row[0]+" v1"
+        if strain in predicted_solutions:
+            exp = row[1]
+            calc = predicted_solutions[strain].fluxes["Ec_biomass_iJO1366_core_53p95M"]
+            exps.append(exp)
+            calcs.append(calc)
+
+    sqsum = sum((np.array(exps)-np.array(calcs))**2)
+    root_mean_sq = (sqsum / len(exps))**0.5
+    return root_mean_sq
+
+
+def calculate_growth_spearman_rank(predicted_solutions, experimental_growth_rate_filename):
+    exps = []
+    calcs = []
+
+    gr_df = pd.read_csv(experimental_growth_rate_filename)
+    for idx, row in gr_df.iterrows():
+        strain = row[0]+" v1"
+        if strain in predicted_solutions:
+            exp = row[1]
+            calc = predicted_solutions[strain].fluxes["Ec_biomass_iJO1366_core_53p95M"]
+            exps.append(exp)
+            calcs.append(calc)
+
+    cor_coef = stats.spearmanr(np.array(exps), np.array(calcs))
+    return cor_coef
+
+
+def find_strain_solutions(model, strains, rel_bounds, solve_method, original_bounds, **kwargs):
+    """
+    :param model:
+    :param strains:
+    :param rel_bounds:
+    :param solve_method:
+    :param original_bounds:
+    :param kwargs:
+    :return:
+    """
+    strain_solutions = {}
+    for strain in strains:
+        strain_rel_bounds = rel_bounds[strain]
+        try:
+            solution = find_mutant_growth_rate(model, strain_rel_bounds, original_bounds=original_bounds,
+                                               solve_method=solve_method, **kwargs)
+        except SolveError as e:
+            sys.stderr.write(strain+" could not be solved ("+str(e)+")\n")
+            continue
+        strain_solutions[strain] = solution
+
+    return strain_solutions
+
+
+def find_initial_bounds_from_c13(model, c13_path, activity_level, reference=None):
+    """
+    :param model:
+    :param c13_path:
+    :param activity_level:
+    :return: Reference flux distribution result and initial_bounds
+    """
+    # Reactions to skip
+    exchanges = [reac.id for reac in model.exchanges]
+    skip_reacs = exchanges + ["ATPM", "Ec_biomass_iJO1366_core_53p95M"]
+
+    c13_bounds = pd.DataFrame.from_csv(c13_path)
+    if reference is None:
+        with TimeMachine() as tm:
+            for r_id in c13_bounds.index:
+                lower = round(c13_bounds["lower_bound"][r_id], 6)
+                upper = round(c13_bounds["upper_bound"][r_id], 6)
+                r = model.reactions.get_by_id(r_id)
+                tm(do=partial(setattr, r, "lower_bound", lower),
+                   undo=partial(setattr, r, "lower_bound", r.lower_bound))
+                tm(do=partial(setattr, r, "upper_bound", upper),
+                   undo=partial(setattr, r, "upper_bound", r.upper_bound))
+
+            reference = cameo.pfba(model)
+
+    with TimeMachine() as tm:
+        for r_id in c13_bounds.index:
+            if r_id in skip_reacs:
+                continue
+            lower = round(c13_bounds["lower_bound"][r_id], 6)
+            upper = round(c13_bounds["upper_bound"][r_id], 6)
+            reac = model.reactions.get_by_id(r_id)
+            if lower <= 0:
+                tm(do=partial(setattr, reac, "lower_bound", lower/activity_level),
+                   undo=partial(setattr, reac, "lower_bound", reac.lower_bound))
+            if upper >= 0:
+                tm(do=partial(setattr, reac, "upper_bound", upper/activity_level),
+                   undo=partial(setattr, reac, "upper_bound", reac.upper_bound))
+
+        biomass = model.reactions.Ec_biomass_iJO1366_core_53p95M
+        #tm(do=partial(setattr, r, "upper_bound", reference["Ec_biomass_iJO1366_core_53p95M"]),
+        #   undo=partial(setattr, r, "upper_bound", biomass.upper_bound))
+        wt_bounds = cameo.flux_variability_analysis(model, fraction_of_optimum=1, remove_cycles=False).data_frame
+        wt_bounds["lower_bound"] = wt_bounds["lower_bound"].round(6)
+        wt_bounds["upper_bound"] = wt_bounds["upper_bound"].round(6)
+        for r_id in c13_bounds.index:
+            if r_id in skip_reacs:
+                continue
+            lower = round(c13_bounds["lower_bound"][r_id], 6)
+            upper = round(c13_bounds["upper_bound"][r_id], 6)
+            if lower <= 0:
+                wt_bounds.loc[r_id, "lower_bound"] = lower / activity_level
+            if upper >= 0:
+                wt_bounds.loc[r_id, "upper_bound"] = upper / activity_level
+        wt_result = reference
+
+        return wt_result, wt_bounds
